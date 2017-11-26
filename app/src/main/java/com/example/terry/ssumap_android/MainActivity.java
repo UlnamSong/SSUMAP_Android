6f3042b543907bc1a9ac3f3479348100bc01cf09
Ulnamsong <gydect48@naver.com>
2017-11-26 13:33:56 +0900

    다음 지도 성공 및 버튼 구성
    다음 지도 성공 및 버튼 구성 완료됨.

--- app/src/main/java/com/example/terry/ssumap_android/MainActivity.java
+++ app/src/main/java/com/example/terry/ssumap_android/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.terry.ssumap_android;
 import android.Manifest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
+
+import net.daum.mf.map.api.MapView;
 
 import java.util.ArrayList;
 
@@ -21,6 +24,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onPermissionGranted() {
                 Toast.makeText(MainActivity.this, "권한 허가", Toast.LENGTH_SHORT).show();
+
+                MapView mapView = new MapView(MainActivity.this);
+                mapView.setDaumMapApiKey("ea8d49685012f33f11397e22230ecba1");
+
+                ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+                mapViewContainer.addView(mapView);
             }
 
             @Override
@@ -31,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         TedPermission.with(this)
                 .setPermissionListener(permissionlistener)
-                .setRationaleMessage("서비스 이용을 위해서는 위치 권한이 필요해요")
+                .setRationaleMessage("서비스 이용을 위해서는 위치 권한이 필요합니다.")
                 .setDeniedMessage("[설정] > [권한] 에서 권한을 허용해 주세요.")
                 .setPermissions(Manifest.permission.ACCESS_FINE_LOCATION)
                 .check();
